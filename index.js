c57830957628223dcac7e3a1e3f16d66680157bc
deeputkarsh007 <101280057+deeputkarsh007@users.noreply.github.com>
2025-08-17 18:11:27 +0530

    Update index.js for cors

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ const test = require("./routes/test");
 // const report = require("./routes/generateReport");
 
 app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
